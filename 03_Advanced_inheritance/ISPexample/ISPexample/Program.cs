10526c4ff625e01224b6a312df86682dfebd40a5
zaticpetru <zatic.petru@outlook.com>
2018-07-05 15:19:07 +0300

    #3.1 update ISP example

---
+++ 03_Advanced_inheritance/ISPexample/ISPexample/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPexample
+{
+    public interface IMotorcicle
+    {
+        int Engine_Displacement();
+        int Horsepower();
+        String GetTotalInformation();
+    }
+
+    public interface ISport
+    {
+        int GetUsedMotohours();
+        int GetWins();
+        int GetFails();
+        int GetTotalParticipations();
+    }
+    
+    public interface IRoad
+    {
+        int GetMileage();
+        int GetYearOfIssue();
+        string GetRegistrationNumber();
+    }
+
+    class KavasakiH2r : IMotorcicle, ISport
+    {
+        public int Engine_Displacement()
+        {
+            return (998);
+        }
+
+        public int GetFails()
+        {
+            return (3);
+        }
+
+        public int GetWins()
+        {
+            return (97);
+        }
+
+        public int GetTotalParticipations()
+        {
+            return (GetWins() + GetFails());
+        }
+
+        public int GetUsedMotohours()
+        {
+            return (0);
+        }
+
+        public int Horsepower()
+        {
+            return (310);
+        }
+
+        public string GetTotalInformation()
+        {
+             return("\nEngine displacement: " + Engine_Displacement()
+                + "\nFails: " + GetFails() + " Wins: " + GetFails() + " Total partitipation:" + GetTotalParticipations()
+                + "\nMotohours used: " + GetUsedMotohours()
+                + "\nHorsepowers: " + Horsepower() + "\n");
+        }
+    }
+
+    class HondaCB500 : IMotorcicle, IRoad
+    {
+        public int Engine_Displacement()
+        {
+            return (500);
+        }
+
+        public int GetMileage()
+        {
+            return (23025);
+        }
+
+        public string GetRegistrationNumber()
+        {
+            return ("QWD 220");
+        }
+
+        public int GetYearOfIssue()
+        {
+            return (1998);
+        }
+
+        public int Horsepower()
+        {
+            return (57);
+        }
+
+        public string GetTotalInformation()
+        {
+            return ("\nEngine displacement: " + Engine_Displacement()
+               + "\nMileage: " + GetMileage() + " Registration number: " + GetRegistrationNumber() + " Year of issue:" + GetYearOfIssue()
+               + "\nHorsepowers: " + Horsepower() + "\n");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HondaCB500 b = new HondaCB500();
+            List<IMotorcicle> list = new List<IMotorcicle>();
+            list.Add(new KavasakiH2r());
+            list.Add(new HondaCB500());
+            list.Add(new KavasakiH2r());
+            list.Add(new KavasakiH2r());
+            list.Add(new HondaCB500());
+            list.Add(new KavasakiH2r());
+            list.Add(new KavasakiH2r());
+            list.Add(new HondaCB500());
+            list.Add(new KavasakiH2r());
+
+            foreach(IMotorcicle moto in list)
+            {
+                Console.WriteLine(moto.GetTotalInformation());
+            }
+
+
+            foreach (IMotorcicle moto in list)
+            {
+                if (moto is ISport s)
+                {
+                    Console.WriteLine(s.GetWins());
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
