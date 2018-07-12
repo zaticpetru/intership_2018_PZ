3af733ee19970aa86321ad8ee4af38e0d955d447
zaticpetru <zatic.petru@outlook.com>
2018-07-12 15:15:16 +0300

    #6.4 updating the ex3

---
+++ 06_Array_String/ex3/ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    class Program
+    {
+
+        static void DublicateChar(ref string s, char c)
+        {
+            int k = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c) k++;
+            }
+            char[] response = new char[s.Length + k];
+
+            k = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c) response[k++] = s[i];
+                response[k++] = s[i];
+            }
+
+            s = new string(response);
+        }
+        static void Main(string[] args)
+        {
+            string test = "sarmisegetusa";
+
+            Console.WriteLine(test);
+            DublicateChar(ref test, 's');
+            Console.WriteLine(test);
+
+            Console.ReadKey();
+        }
+    }
+}
