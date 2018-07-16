17222c626afbe65aeb6bbb6bda96ce1d8db5077c
zaticpetru <zatic.petru@outlook.com>
2018-07-16 12:02:52 +0300

    #7.2 first commit  of task solution

---
+++ 07_Overloading_Interfaces/7_overloading/7_overloading/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_overloading
+{
+
+    class Angle{
+        private int degrees;
+        private int minutes;
+        private int seconds;
+
+
+        public Angle(int deg, int min, int sec) {
+            degrees = deg;
+            minutes = min;
+            seconds = sec;
+            ToNormal();
+        }
+
+        private int this[int i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 0: return seconds;
+                    case 1: return minutes;
+                    case 2: return degrees;
+                    default: throw new IndexOutOfRangeException("Attemp to retrive element " + i);
+                }
+            }
+            set
+            {
+                switch (i)
+                {
+                    case 0: seconds = value; break;
+                    case 1: minutes = value; break;
+                    case 2: degrees = value; break;
+                    default: throw new IndexOutOfRangeException("Attemp to set element " + i);
+                }
+            }
+        }
+
+        public static Angle operator+(Angle l, Angle r)
+        {
+            return new Angle(l.degrees + r.degrees, l.minutes + r.minutes, l.seconds + r.seconds);
+        }
+
+        public static Angle operator -(Angle l, Angle r)
+        {
+            return new Angle(l.degrees - r.degrees, l.minutes - r.minutes, l.seconds - r.seconds);
+        }
+
+        public static Angle operator *(Angle l, int c)
+        {
+            return new Angle(l.degrees * c, l.minutes * c, l.seconds * c);
+        }
+
+        public static Angle operator /(int c, Angle l)
+        {
+            return new Angle(l.degrees / c, l.minutes / c, l.seconds / c);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            yield return this[0];
+            yield return this[1];
+            yield return this[2];
+        }
+        private void ToNormal() {
+            for(int i = 0; i < 2; i++)
+            {
+                if(this[i] / 60 != 0)
+                {
+                    this[i + 1] += this[i] / 60;
+                    this[i] %= 60;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return (degrees + "° " + minutes + "\' " + seconds + "\"");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle a = new Angle(0, 0, -30);
+            Angle b = new Angle(0, -0, -31);
+            //a.ToNormal();
+            Console.WriteLine((a + b).ToString());
+            Console.ReadKey();
+        }
+    }
+}
