3067ef29e29e0684f4274094c409693c014cb097
zaticpetru <zatic.petru@outlook.com>
2018-07-16 12:55:19 +0300

    #7.4 adding Car and IComparable

--- 07_Overloading_Interfaces/7_overloading/7_overloading/Program.cs
+++ 07_Overloading_Interfaces/7_overloading/7_overloading/Program.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace _7_overloading
 {
 
-    class Angle{
+    class 
+        Angle{
         private int degrees;
         private int minutes;
         private int seconds;
@@ -94,6 +95,79 @@ namespace _7_overloading
             return (degrees + "° " + minutes + "\' " + seconds + "\"");
         }
     }
+
+    class Car : IComparable
+    {
+        public string name { get; set; }
+        public int speed { get; set; }
+        public int year { get; set; }
+
+        public Car()
+        {
+            name = "Unknown";
+            speed = -1;
+            year = -1;
+        }
+
+        public Car(string name, int speed, int year)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.year = year;
+        }
+
+        public static IComparer SortByYear
+        {
+            get
+            {
+                return new YearComparer();
+            }
+        }
+
+        public static IComparer SortByName
+        {
+            get
+            {
+                return new NameComparer();
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Car ToCompare = (Car) obj;
+            if (this.speed > ToCompare.speed) return 1;
+            if (this.speed < ToCompare.speed) return -1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return ("Name = " + name + " Speed = " + speed + " Year = " + year);
+        }
+    }
+
+    class YearComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            Car l = (Car) x;
+            Car r = (Car) y;
+            if (l.year > r.year) return 1;
+            if (l.year < r.year) return -1;
+            return 0;
+        }
+    }
+    class NameComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            Car l = (Car)x;
+            Car r = (Car)y;
+            return String.Compare(l.name, r.name);
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -102,6 +176,42 @@ namespace _7_overloading
             Angle b = new Angle(0, -0, -31);
             //a.ToNormal();
             Console.WriteLine((a + b).ToString());
+
+            Car[] test = new Car[10];
+
+            Random r = new Random();
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = new Car((r.Next()%100).ToString(), 2 + r.Next() % 100, 3 + r.Next() % 100);
+            }
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i].ToString());
+            }
+
+            Console.WriteLine("\n\nSortbyName:");
+            Array.Sort(test, Car.SortByName);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i].ToString());
+            }
+
+            Console.WriteLine("\n\nSortbyYear:");
+            Array.Sort(test, Car.SortByYear);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i].ToString());
+            }
+
+            Console.WriteLine("\n\nSortbySpeed:");
+            Array.Sort(test);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i].ToString());
+            }
+
             Console.ReadKey();
         }
     }
