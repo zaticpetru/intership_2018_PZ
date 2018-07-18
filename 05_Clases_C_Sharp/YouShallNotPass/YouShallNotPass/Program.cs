737aa7662a4ee7df45f8ce877afdab139fa208ce
zaticpetru <zatic.petru@outlook.com>
2018-07-18 16:30:01 +0300

    #5.2 adding enums and nullable to class example

--- 05_Clases_C_Sharp/YouShallNotPass/YouShallNotPass/Program.cs
+++ 05_Clases_C_Sharp/YouShallNotPass/YouShallNotPass/Program.cs
@@ -6,16 +6,81 @@ using System.Threading.Tasks;
 
 namespace YouShallNotPass
 {
+    public enum MotoType {
+        Mini = 50,
+        Small = 125,
+        Medium = 400,
+        Big = 600,
+        Heavy = 1000,
+        ExtraHeavy = 1200
+    }
     public abstract class Motorcicle{
 
         protected int ProductionYear;
 
         protected int EngineDisplacement;
 
+        protected MotoType T;
+
+        public string GetTypeOfMoto()
+        {
+            switch (T){
+                case MotoType.Mini: return nameof(MotoType.Mini);
+                case MotoType.Small: return nameof(MotoType.Small);
+                case MotoType.Medium: return nameof(MotoType.Medium);
+                case MotoType.Big: return nameof(MotoType.Big);
+                case MotoType.Heavy: return nameof(MotoType.Heavy);
+                case MotoType.ExtraHeavy: return nameof(MotoType.ExtraHeavy);
+                default: return "unknown";
+            }
+        }
+
+        public void SetTypeOfMoto()
+        {
+            if (EngineDisplacement < (int)MotoType.Mini) T = MotoType.Mini;
+            else if (EngineDisplacement < (int)MotoType.Small) T = MotoType.Small;
+            else if (EngineDisplacement < (int)MotoType.Medium) T = MotoType.Medium;
+            else if (EngineDisplacement < (int)MotoType.Big) T = MotoType.Big;
+            else if (EngineDisplacement < (int)MotoType.Heavy) T = MotoType.Heavy;
+            else T = MotoType.ExtraHeavy;
+
+        }
+
         protected string ModelName;
         public int Age
         {
             get { return DateTime.Now.Year - ProductionYear; }
+        }
+
+        protected DateTime? LastBuyDate = null;
+        protected DateTime? LastSellDate = null;
+
+        public void BuyMoto()
+        {
+            if (LastBuyDate == null)
+                Console.WriteLine("Congratulation, you are the first buyer!!!!");
+            LastBuyDate = DateTime.Now;
+        }
+
+        public void SellMoto()
+        {
+            if (LastSellDate == null)
+                Console.WriteLine("Congratulation, you are the first seller!!!!");
+            LastSellDate = DateTime.Now;
+        }
+
+        public string GetLastBuyDate()
+        {
+            if (LastBuyDate.HasValue)
+                return Convert.ToString(LastBuyDate);
+            else return "Motorcicle was not selled yet!";
+        }
+
+        public string GetLastSellDate()
+        {
+            if (LastSellDate.HasValue)
+                return Convert.ToString(LastSellDate);
+            else return "Motorcicle was not buyed yet!";
         }
 
         protected double mileage = 0;
@@ -78,6 +143,7 @@ namespace YouShallNotPass
             this.ModelName = ModelName;
             this.ProductionYear = ProductionYear;
             this.EngineDisplacement = EngineDisplacement;
+            SetTypeOfMoto();
         }
 
         private DateTime NextTC = DateTime.Now.AddDays(60);
@@ -114,7 +180,7 @@ namespace YouShallNotPass
         public override string GetTotalInformation()
         {
             return ModelName + " produced in " + ProductionYear + ", days until next tehnical revision " + GetDaysUntilNextTC() 
-                 + "\n you can buy NOW AND HERE!!! -> " + GetPrice() + " $\nA one year maintenance cost: " + YearMaintenancePrice() + "\nTotal mileage = " + mileage;
+                 + "\n you can buy NOW AND HERE!!! -> " + GetPrice() + " $\nA one year maintenance cost: " + YearMaintenancePrice() + "\nTotal mileage = " + mileage + " Type of moto: " + GetTypeOfMoto();
         }
     }
     class Program
@@ -123,7 +189,7 @@ namespace YouShallNotPass
         static void Main(string[] args)
         {
             Sport kawa = Sport.Create("Kawasakii H2r", 2014, 1099);
-            Road honda = Road.Create("Honda CB500", 1998, 500);
+            Road honda = Road.Create("Honda CB500", 1998, 999);
 
             Console.WriteLine(kawa.GetTotalInformation() + "\n\n");
             Console.WriteLine(honda.GetTotalInformation()+ "\n\n");
