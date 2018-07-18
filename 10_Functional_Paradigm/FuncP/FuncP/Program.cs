873992b6beac109b29feddad4178d87bab95459f
zaticpetru <zatic.petru@outlook.com>
2018-07-18 18:16:00 +0300

    #10.2 first commit of class

---
+++ 10_Functional_Paradigm/FuncP/FuncP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncP
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public Person() { }  
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
